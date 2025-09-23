5825ddd017fc58921e23cfb413f6acb70093d7d4
tnwl17633 <tnwl17633@gmail.com>
2025-09-23 17:47:12 +0900

    feat: 20250923 h2 / mariadb / jpa join / REST / postman

--- cardatabase/src/main/java/com/example/cardatabase/domain/Owner.java
+++ cardatabase/src/main/java/com/example/cardatabase/domain/Owner.java
@@ -1,11 +1,14 @@
 package com.example.cardatabase.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.List;
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Getter
 @Setter
 @ToString
@@ -21,7 +24,8 @@ public class Owner {
     private final String lastName;
 
     // 소유자는 다수의 차들을 가질 수 있기 때문에 Collections를 사용.
+    @JsonIgnore
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private List<Car> cars;
-
 }
+
---
+++ cardatabase/src/main/java/com/example/cardatabase/web/CarController.java
@@ -0,0 +1,21 @@
+package com.example.cardatabase.web;
+
+import com.example.cardatabase.domain.Car;
+import com.example.cardatabase.domain.CarRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CarController {
+    private final CarRepository carRepository;
+
+    public CarController(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    @GetMapping("/cars")
+    public Iterable<Car> getCars() {
+        //복잡하게 할 거 없이 자동차들이 저장된 테이블에서 전체 명단을 가지고 올겁니다.
+        return carRepository.findAll();
+    }
+}
